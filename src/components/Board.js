c16070aec5e6c2fec86d11deb25803a184471995
kbsjsn <shin.kirk@gmail.com>
2020-03-25 13:02:20 -0700

    basic requirement for setting pieces at rows

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,89 @@
 import React from 'react';
 import './Board.css';
 
+const redPiece = (
+  <div className="red-piece"></div>
+);
+
+const blackPiece = (
+  <div className="black-piece"></div>
+);
+
+const blackCell = (key, piece = null) => (
+  <div className="black-cell" key={key}>{piece}</div>
+);
+
+const whiteCell = (key, piece = null) => (
+  <div className="white-cell" key={key}>{piece}</div>
+);
+
 function makeRows (cells) {
   const board = [];
   for (let i = 0; i < cells; i++) {
     const row = [];
-    if (i % 2 === 0) {
-      for (let j = 0; j < cells; j++) {
-        if (j % 2 === 0) {
-          row.push(<div className="black-cell" key={j}></div>)
-        } 
-        else {
-          row.push(<div className="white-cell" key={j}></div>)
+    if (i < 2) {
+      if (i % 2 === 0) {
+        for (let j = 0; j < cells; j++) {
+          if (j % 2 === 0) {
+            row.push(blackCell(j, redPiece))
+          } 
+          else {
+            row.push(whiteCell(j, redPiece))
+          }
+        }
+      }
+      else {
+        for (let j = 0; j < cells; j++) {
+          if (j % 2 === 0) {
+            row.push(whiteCell(j, redPiece))
+          } 
+          else {
+            row.push(blackCell(j, redPiece))
+          }
+        }
+      }
+    }
+    else if (i >= cells - 2) {
+      if (i % 2 === 0) {
+        for (let j = 0; j < cells; j++) {
+          if (j % 2 === 0) {
+            row.push(blackCell(j, blackPiece))
+          } 
+          else {
+            row.push(whiteCell(j, blackPiece))
+          }
+        }
+      }
+      else {
+        for (let j = 0; j < cells; j++) {
+          if (j % 2 === 0) {
+            row.push(whiteCell(j, blackPiece))
+          } 
+          else {
+            row.push(blackCell(j, blackPiece))
+          }
         }
       }
     }
     else {
-      for (let j = 0; j < cells; j++) {
-        if (j % 2 === 0) {
-          row.push(<div className="white-cell" key={j}></div>)
-        } 
-        else {
-          row.push(<div className="black-cell" key={j}></div>)
+      if (i % 2 === 0) {
+        for (let j = 0; j < cells; j++) {
+          if (j % 2 === 0) {
+            row.push(blackCell(j))
+          } 
+          else {
+            row.push(whiteCell(j))
+          }
+        }
+      }
+      else {
+        for (let j = 0; j < cells; j++) {
+          if (j % 2 === 0) {
+            row.push(whiteCell(j))
+          } 
+          else {
+            row.push(blackCell(j))
+          }
         }
       }
     }
--- src/components/Input.js
+++ src/components/Input.js
@@ -5,6 +5,10 @@ export default function Input ({ setCells }) {
   return (
     <div className="inputbar">
       <input onChange={e => setCells(e.target.value)} />
+      <input type="radio" id="red-piece" name="color" value="red"></input>
+      <label for="redPiece">Red</label>
+      <input type="radio" id="black-piece" name="color" value="black"></input>
+      <label for="blackPiece">Black</label>
     </div>
   )
 }
