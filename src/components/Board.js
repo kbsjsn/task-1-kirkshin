13aae3742d65415ecd0487bd08fb0a8a0e527122
kbsjsn <shin.kirk@gmail.com>
2020-03-25 15:56:04 -0700

    implement feat to display next moves with blue background

--- src/components/Board.js
+++ src/components/Board.js
@@ -24,7 +24,48 @@ const makeBoard = (cells) => {
 export default function Board ({ cells, pieceColor, pieceShape }) {
   const board = makeBoard(cells);
 
-  const [selectedPiece, setSelectedPiece] = useState();
+  const [selectedPiece, setSelectedPiece] = useState(); // selectedPiece is string rep board arr indexes
+  const [nextMoveLeft, setNextMoveLeft] = useState()
+  const [nextMoveRight, setNextMoveRight] = useState()
+
+  if (selectedPiece) {
+    let nextRow;
+    const currCol = +selectedPiece[1]
+    if (board[selectedPiece[0]][selectedPiece[1]] === 'top') {
+      // check bottom diagonals
+      nextRow = +selectedPiece[0] + 1;
+      if ((board[nextRow][currCol + 1] || board[nextRow][currCol + 1] === undefined) && nextMoveRight !== null) {
+        setNextMoveRight(null)
+      }
+      if ((board[nextRow][currCol - 1] || board[nextRow][currCol - 1] === undefined) && nextMoveLeft !== null) {
+        setNextMoveLeft(null)
+      }
+    }
+    if (board[selectedPiece[0]][selectedPiece[1]] === 'bottom') {
+      nextRow = +selectedPiece[0] - 1;
+      // check top diagonals
+      if ((board[nextRow][currCol + 1] || board[nextRow][currCol + 1] === undefined) && nextMoveRight !== null) {
+        setNextMoveRight(null)
+      }
+      if ((board[nextRow][currCol - 1] || board[nextRow][currCol - 1] === undefined) && nextMoveLeft !== null) {
+        setNextMoveLeft(null)
+      }
+    }
+    if (board[nextRow][currCol + 1] === null && `${nextRow}${currCol + 1}` !== nextMoveRight) {
+      setNextMoveRight(`${nextRow}${currCol + 1}`)
+    }
+    if (board[nextRow][currCol - 1] === null && `${nextRow}${currCol - 1}` !== nextMoveLeft) {
+      setNextMoveLeft(`${nextRow}${currCol - 1}`);
+    }
+    // else {
+    //   if (nextMoveRight) {
+    //     setNextMoveRight(null)
+    //   }
+    //   if (nextMoveLeft) {
+    //     setNextMoveRight(null)
+    //   }
+    // }
+  }
 
   const renderBoard = (board, pieceColor, pieceShape) => {
     return board.map((row, i) => <div className="row" key={i}>
@@ -32,7 +73,13 @@ export default function Board ({ cells, pieceColor, pieceShape }) {
         row.map((cell, j) => {
             if ((cell === 'top' && pieceColor === 'red') || (cell === 'bottom' && pieceColor === 'black')) {
               return (
-                <div className={`${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}`} key={`${i}${j}`} >
+                <div 
+                  className={`
+                    ${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}
+                    ${(nextMoveLeft === `${i}${j}` || nextMoveRight === `${i}${j}`) && 'next-move'}
+                  `} 
+                  key={`${i}${j}`} 
+                >
                   <div className={`red-piece 
                       ${pieceShape === 'circle' ? 'circle' : 'square'}
                       ${selectedPiece === `${i}${j}` && 'selected-piece'}
@@ -44,7 +91,13 @@ export default function Board ({ cells, pieceColor, pieceShape }) {
             }
             if ((cell === 'top' && pieceColor === 'black') || (cell === 'bottom' && pieceColor === 'red')) {
               return (
-                <div className={`${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}`} key={`${i}${j}`} >
+                <div 
+                  className={`
+                    ${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}
+                    ${(nextMoveLeft === `${i}${j}` || nextMoveRight === `${i}${j}`) && 'next-move'}
+                  `} 
+                  key={`${i}${j}`} 
+                >
                   <div 
                     className={`black-piece 
                       ${pieceShape === 'circle' ? 'circle' : 'square'}
@@ -56,7 +109,13 @@ export default function Board ({ cells, pieceColor, pieceShape }) {
               )
             }
             return (
-              <div className={`${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}`} key={`${i}${j}`}>
+              <div 
+                className={`
+                  ${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}
+                  ${(nextMoveLeft === `${i}${j}` || nextMoveRight === `${i}${j}`) && 'next-move'}
+                `} 
+                key={`${i}${j}`}
+              >
               </div>
             )
           }
