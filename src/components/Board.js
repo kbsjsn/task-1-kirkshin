5a98f48d8d3df2878293ba9df5c807db275e8a14
kbsjsn <shin.kirk@gmail.com>
2020-03-25 14:09:15 -0700

    refactored Board component to display model of board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,105 +1,62 @@
 import React from 'react';
 import './Board.css';
 
-export default function Board ({ cells, pieceColor, pieceShape }) {
-
-  const redPiece = (
-    <div className={`red-piece + ${pieceShape === 'circle' ? 'circle' : 'square'}`}></div>
-  );
-  
-  const blackPiece = (
-    <div className={`black-piece + ${pieceShape === 'circle' ? 'circle' : 'square'}`}></div>
-  );
-  
-  const blackCell = (key, piece = null) => (
-    <div className="black-cell" key={key}>{piece}</div>
-  );
-  
-  const whiteCell = (key, piece = null) => (
-    <div className="white-cell" key={key}>{piece}</div>
-  );
-  
-  function makeRows (cells) {
-    const board = [];
-    for (let i = 0; i < cells; i++) {
-      const row = [];
-      if (i < 2) {
-        let piece = pieceColor === 'red' ? redPiece : blackPiece;
-        if (i % 2 === 0) {
-          for (let j = 0; j < cells; j++) {
-            if (j % 2 === 0) {
-              row.push(blackCell(j, piece))
-            } 
-            else {
-              row.push(whiteCell(j, piece))
-            }
-          }
-        }
-        else {
-          for (let j = 0; j < cells; j++) {
-            if (j % 2 === 0) {
-              row.push(whiteCell(j,  piece))
-            } 
-            else {
-              row.push(blackCell(j, piece))
-            }
-          }
-        }
+const makeBoard = (cells) => {
+  const board = [];
+  for (let i = 0; i < cells; i++) {
+    const row = [];
+    for (let j = 0; j < cells; j++) {
+      if (i < 2) { 
+        row.push('top')
       }
       else if (i >= cells - 2) {
-        let piece = pieceColor === 'red' ? blackPiece : redPiece;
-        if (i % 2 === 0) {
-          for (let j = 0; j < cells; j++) {
-            if (j % 2 === 0) {
-              row.push(blackCell(j, piece))
-            } 
-            else {
-              row.push(whiteCell(j, piece))
-            }
-          }
-        }
-        else {
-          for (let j = 0; j < cells; j++) {
-            if (j % 2 === 0) {
-              row.push(whiteCell(j, piece))
-            } 
-            else {
-              row.push(blackCell(j, piece))
-            }
-          }
-        }
+        row.push('bottom')
       }
       else {
-        if (i % 2 === 0) {
-          for (let j = 0; j < cells; j++) {
-            if (j % 2 === 0) {
-              row.push(blackCell(j))
-            } 
-            else {
-              row.push(whiteCell(j))
-            }
-          }
-        }
-        else {
-          for (let j = 0; j < cells; j++) {
-            if (j % 2 === 0) {
-              row.push(whiteCell(j))
-            } 
-            else {
-              row.push(blackCell(j))
-            }
-          }
-        }
+        row.push(null)
       }
-      board.push(row)
     }
-    return board;
+    board.push(row)
   }
+  return board
+}
+
+const renderBoard = (board, pieceColor, pieceShape) => {
+  return board.map((row, i) => <div className="row" key={i}>
+    {
+      row.map((cell, j) => {
+          if (cell === 'top' && pieceColor === 'red' || cell === 'bottom' && pieceColor === 'black') {
+            return (
+              <div className={`${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}`} key={`${i}${j}`} >
+                <div className={`red-piece ${pieceShape === 'circle' ? 'circle' : 'square'}`} ></div>
+              </div>
+            )
+          }
+          if (cell === 'top' && pieceColor === 'black' || cell === 'bottom' && pieceColor === 'red') {
+            return (
+              <div className={`${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}`} key={`${i}${j}`} >
+                <div className={`black-piece ${pieceShape === 'circle' ? 'circle' : 'square'}`} ></div>
+              </div>
+            )
+          }
+          return (
+            <div className={`${(i + j) % 2 === 0 ? 'black-cell' : 'white-cell'}`} key={`${i}${j}`}>
+            </div>
+          )
+        }
+      )
+    }
+    </div>  
+  )
+}
+
+export default function Board ({ cells, pieceColor, pieceShape }) {
+  const board = makeBoard(cells);
 
   return (
     <div className="board">
       {
-        makeRows(cells).map(row => <div className="row">{row}</div>)
+        renderBoard(board, pieceColor, pieceShape)
       }
     </div>
   )
